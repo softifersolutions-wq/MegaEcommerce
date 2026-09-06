9161a1d3c28a8ef98606368e6a5469ca4a96f729
softifersolutions-wq <softifer.solutions@gmail.com>
2026-09-06 13:30:07 +0500

    Update OrderController.java

--- src/main/java/com/electromart/ecommerce/controller/OrderController.java
+++ src/main/java/com/electromart/ecommerce/controller/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/reg/orders")
 @RequiredArgsConstructor
+    @CrossOrigin("*")
 public class OrderController {
 
     private final OrderService orderService;
