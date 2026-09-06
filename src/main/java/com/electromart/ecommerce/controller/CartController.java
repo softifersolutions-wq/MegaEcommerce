c4011b1d95ddfeda4269ebd06e07193847979e64
softifersolutions-wq <softifer.solutions@gmail.com>
2026-09-06 13:29:44 +0500

    Update CartController.java

--- src/main/java/com/electromart/ecommerce/controller/CartController.java
+++ src/main/java/com/electromart/ecommerce/controller/CartController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/reg/cart")
 @RequiredArgsConstructor
+    @CrossOrigin("*")
 public class CartController {
 
     private final CartService cartService;
