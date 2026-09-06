84a339d403a3d8aab493046b0e58e905458ff1ab
softifersolutions-wq <softifer.solutions@gmail.com>
2026-09-06 13:30:26 +0500

    Update ProductController.java

--- src/main/java/com/electromart/ecommerce/controller/ProductController.java
+++ src/main/java/com/electromart/ecommerce/controller/ProductController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/public/products")
 @RequiredArgsConstructor
+    @CrossOrigin("*")
 public class ProductController {
 
     private final ProductService productService;
