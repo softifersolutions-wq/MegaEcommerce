a73bd56a6effaaf100958f4f028112a0539f3300
softifersolutions-wq <softifer.solutions@gmail.com>
2026-09-12 13:00:33 +0500

    Delete src directory

--- src/main/java/com/electromart/ecommerce/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.electromart.ecommerce.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
